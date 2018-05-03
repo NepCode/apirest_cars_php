5a1e29353929ef66d8f2e21be3020f4c98c58e38
Jose Antonio Aguila Rojas <raiony91@live.com>
2018-05-03 18:32:27 -0500

    actualizacion error

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Helpers\JwtAuth;
+use App\Car;
 
 
 class CarController extends Controller
@@ -22,4 +23,38 @@ class CarController extends Controller
             echo "INDEX DE CARCONTROLLER NO AUTENTICADO"; die();
         }
     }
+
+
+    public function store(Request $request){
+        $hash = $request->header('Authorization',null);
+
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken){
+
+            //obtener datos por post
+            $json = $request->input('json',null);
+            $params = json_decode($json);
+
+            //conseguir usuario identificado
+            $user = $jwtAuth->checkToken($hash,true);
+
+            //guaradar el coche
+
+            if(isset($params->title) && isset($params->description) && isset($params->price) && isset($params->status)) {
+                $car = new Car();
+                $car->title = $params->title;
+                $car->description = $params->description;
+                $car->price = $params->price;
+                $car->status = $params->status;
+
+                $car->save();
+            }
+        }else {
+            //devolver error
+        }
+    }
+
+
 }
